06f4780a9d51e48060320f4ab0a97e7ec1866b86
shatha9522 <shatha_abuali95@hotmail.com>
2020-12-23 15:14:47 +0200

    lab06

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = prompt('Enter your name' );
+var season = prompt('Enter season');
+var message;
+
+if(season=summer){
+    message = 'summer';
+}
+ else{ 
+     message = 'winter';
+ }
+alert(message);
