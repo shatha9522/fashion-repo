1047e02f375f6a5de7a92f45642a240c77ee9276
shatha9522 <shatha_abuali95@hotmail.com>
2020-12-24 17:08:27 +0200

    newedit

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-var name = prompt('Enter your name' );
-var season = prompt('Enter season');
-var message;
-
-if(season=summer){
-    message = 'summer';
-}
- else{ 
-     message = 'winter';
+ 
+ 
+ var name = prompt('Enter your name' );
+ while (name ===''){
+     name = prompt('Enter your name');
  }
-alert(message);
+ var gender = prompt('Enter your gender');
+  var message;
+
+ if(gender === 'female')
+ {
+     message = 'women';
+     alert(message);
+ }
+  else  { 
+      message ='Go Home';
+      alert(message);
+  }
+ var count =prompt('please Enter the number of pic');
+ for(var i =0;i < count; i++){
+    
+    document.write(i+"<img src='https://assets.ajio.com/medias/sys_master/root/hff/h07/16211011534878/-1117Wx1400H-461149056-pink-MODEL.jpg'>")
+     console.log(count)
+ }
