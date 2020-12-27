623574b545d5c57dd4cb0500f43de548e783d1f3
shatha9522 <shatha_abuali95@hotmail.com>
2020-12-27 15:27:25 +0200

    new

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
  
  
- var name = prompt('Enter your name' );
+ var name = takeUserInput('Enter your Name');
  while (name ===''){
-     name = prompt('Enter your name');
+     name = takeUserInput('Enter your name');
  }
- var gender = prompt('Enter your gender');
+ var gender = takeUserInput('Enter your gender');
   var message;
 
  if(gender === 'female')
@@ -16,9 +16,12 @@
       message ='Go Home';
       alert(message);
   }
- var count =prompt('please Enter the number of pic');
+ var count =takeUserInput('please Enter the number of pic');
  for(var i =0;i < count; i++){
     
     document.write(i+"<img src='https://assets.ajio.com/medias/sys_master/root/hff/h07/16211011534878/-1117Wx1400H-461149056-pink-MODEL.jpg'>")
      console.log(count)
+ }
+ function takeUserInput(message){
+return prompt(message);
  }
